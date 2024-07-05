ac3325a54cc675e0027294680c133940ab86ac99
ClintonS0004 <csib0004@student.monash.edu>
2024-07-05 18:09:11 +1000

    working in the changes from 1.19.0 to 1.21.1

--- src/main/java/com/Clinton/tutorialexample/item/ModItems.java
+++ src/main/java/com/Clinton/tutorialexample/item/ModItems.java
@@ -1,19 +1,27 @@
 package com.Clinton.tutorialexample.item;
 
 import com.Clinton.tutorialexample.TutorialMod;
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
 
-    public static final Item RAW_MODBLOCK = registerItem("raw_modblock",
-            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+    public static Identifier id(String path) {
+        return Identifier.of(TutorialMod.MOD_ID, path);
+    }
 
-    private static Item resigterItem(String name, Item item){
-        return Registry.register(Registries.ITEM , new Identifier(TutorialMod.MOD_ID, name), item);
+    public static final Item RAW_MODBLOCK = registerItem("raw_modblock",
+            new Item(new Item.Settings()));
+    public static final Item MODBLOCK = registerItem("modblock",
+            new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item){
+        return Registry.register(Registries.ITEM,
+                id(name),
+                item);
     }
 //    private static Item registerItem(String name, Item item, ItemGroup group) {
 //        ItemGroupEvents.modifyEntriesEvent(group).register(entries -> entries.add(item));
